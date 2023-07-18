2a8d2b37f6557cc08ac20acd7884ff0d68f0cfce
Viilhov <133661770+Viilhov@users.noreply.github.com>
2023-07-18 13:09:27 +0200

    Added useEffect to update the forcast with correct date

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import WeatherForecastDay from "./WeatherForecastDay";
 import axios from "axios";
@@ -6,6 +6,11 @@ import axios from "axios";
 export default function WeatherForecast(props) {
   const [loaded, setLoaded] = useState(false);
   const [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+    return () => {};
+  }, [props.city]);
 
   function handleResponse(response) {
     setForecast(response.data.daily);
